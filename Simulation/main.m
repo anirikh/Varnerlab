b7f3da41cb2494f6a8507946f2ea1a73eb352fe6
akivasoftware <jdv27@cornell.edu>
2011-06-02 10:51:52 -0400

    Initial Commit

---
+++ Simulation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Simulation
+//
+//  Created by Jeffrey Varner on 6/2/11.
+//  Copyright 2011 Chemical and Biomolecular Engineering. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
